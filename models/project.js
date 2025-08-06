75c0c1b6b05fc5c81819862fd9d1038b714eebba
Sam <sharrisdev@gmail.com>
2025-08-06 09:56:55 +0100

    defined project model

---
+++ models/project.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const projectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' , required: true },
+  description: { type: String, required: true },
+  goalAmount: { type: Number, required: true },
+  currentAmount: { type: Number, required: true, default: 0 },
+  deadline: { type: Date, required: true }
+}, {
+  timestamps: true
+})
+
+const Project = mongoose.model('Project', projectSchema)
+
+export default Project
